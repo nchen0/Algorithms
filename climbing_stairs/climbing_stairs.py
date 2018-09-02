13b9292f50179297a67a655c574de6f5285522ca
Nicky Chen <nicky.chen0@gmail.com>
2018-09-02 14:10:55 -0700

    climbing_stairs done

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python
 
 import sys
+import itertools
+
 
 def climbing_stairs(n):
-  pass
+    if n == 0:
+        return 1
+    original_list = []
+
+# Find ALL combinations of 1,2,3 possible, with repeats, of n times and add it to original_list.
+    for i in range(1, n+1):
+        original_list.append(list(itertools.product('123', repeat=i)))
+
+# Extract each nested list, and put them as integers.
+    extracted_list = []
+    for nested_list in original_list:
+        for inner_list in nested_list:
+            extracted_list.append((list(map(int, inner_list))))
+
+# Return any list combination that adds up to n.
+    return len([sum(j) for j in extracted_list if sum(j) == n])
 
 
 if __name__ == "__main__":
-  # Test out your implementation from the command line
-  # with `python climbing_stairs.py [n]` with different n values
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    # Test out your implementation from the command line
+    # with `python climbing_stairs.py [n]` with different n values
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
