aeedf051406d5d2d25562962ca8f23515a2926b9
Nicky Chen <nicky.chen0@gmail.com>
2018-09-01 18:35:28 -0700

    stock_prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,16 +2,26 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass 
+    # num = float("inf")
+    new_list = []
+    for index, price in enumerate(prices):
+        for i in range(index, len(prices)):
+            new_list.append(prices[i] - price)
+    new_list = [x for x in new_list if x != 0]
+    return max(new_list)
 
 
 if __name__ == '__main__':
-  # You can test your implementation by running 
-  # `python stock_prices.py [prices]` where prices is comprised of
-  # space-separated integer values
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # You can test your implementation by running
+    # `python stock_prices.py [prices]` where prices is comprised of
+    # space-separated integer values
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
